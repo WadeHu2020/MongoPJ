bf9b93dd858055c4cfc22f15ef9e8dc9bb9dbd80
WadeHu2020 <huwei11huwei@gmail.com>
2022-10-09 21:09:52 -0700

    Create test.java
    test the fail situation

---
+++ src/test/java/test.java
@@ -0,0 +1,29 @@
+import org.assertj.core.api.Fail;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test {
+
+    @Test
+    void testPass(){
+        //success
+        assertEquals(1,1);
+    }
+
+    @Test
+    void testFail(){
+        //fail
+        assertEquals(1,2);
+    }
+
+
+
+
+
+
+
+
+
+}
